319b1d30667b08a63aa903111e2e343733612cfd
Prayag Gupta <guptaprayag16@gmail.com>
2025-03-16 01:25:52 +0530

    Fix: Remove empty sitemap.xml directory and unused sitemap file

--- src/app/utils/index.js
+++
@@ -1,8 +0,0 @@
-// Re-export sitemap from a plain JavaScript file
-import sitemapData from './sitemap.js';
-
-// Export for direct imports from index.js
-export const sitemap = sitemapData;
-
-// Default export for standard imports
-export default sitemapData; 
